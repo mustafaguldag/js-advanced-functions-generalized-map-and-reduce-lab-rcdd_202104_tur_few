2eed4794a54d8102dbe24f71ef74df48620b33e4
mustafaguldag <guldag.mustafaa@gmail.com>
2021-05-15 16:51:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your functions here
+let theArray = [1,2,3];
+
+function map(array,func){
+    let newArr = [];
+
+    for (let i=0;i < array.length;i++){
+        newArr.push(func(array[i]))
+    }
+    return newArr;
+
+}
+    
+map(theArray,function(number){
+    return number * -1;
+
+}
+);
+map(theArray,function(number){
+   return number * 2;
+})
+
+function reduce(array, func, startingPoint = 0 ) {
+    let updatedResult = startingPoint
+    
+    for (let i=0;i < array.length;i++){
+        
+        if(array[i] === false){
+            updatedResult = false;
+            continue
+        } else if (array[i] === true){
+            updatedResult = true;
+            continue
+        }
+        updatedResult = func(array[i],updatedResult);
+    }
+  return updatedResult
+}
